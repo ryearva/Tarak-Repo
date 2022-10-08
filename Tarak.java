8a8a6c0dc20be1377ad3d17670fd229a0bf7b8ad
ryearva <rramanareddy28@gmail.com>
2022-10-08 09:43:43 +0530

    created tarak file

---
+++ Tarak.java
@@ -0,0 +1,15 @@
+public class Tarak {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Ramana");
+		int x = 0;
+		if (x == 0) {
+			System.out.println("I am IF Block");
+		} else {
+			System.out.println("i am Else Block");
+			System.out.println("Added new lly");
+		}
+	}
+
+}
